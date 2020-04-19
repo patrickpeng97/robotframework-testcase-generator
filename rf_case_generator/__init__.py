0746bffefe9a69d69bcc324705c8aa6cfd2f9fb6
pengjiahao97 <pengjiahao97@gmail.com>
2020-04-19 23:18:26 +0800

    upgrade to rf_case_generator v1.0.1

--- rf_case_generator/__init__.py
+++ rf_case_generator/__init__.py
@@ -1,4 +1,4 @@
-from scripts.caseProducer import write_case_to_rf
-from scripts.excelReader import read_case_from_excel
+from rf_case_generator.caseProducer import write_case_to_rf
+from rf_case_generator.excelReader import read_case_from_excel
 
 __all__ = ["write_case_to_rf", "read_case_from_excel"]
